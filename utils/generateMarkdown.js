6b23263d0df263b178e0dac3166cd725b8ddde71
Maria Cardona <mechas8703@hotmail.com>
2022-01-20 19:04:09 -0500

    README file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,23 +1,28 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data, githubInfo) {
   return `
 # **${data.title}**
+
 ${data.badge}
+
 ## Description 
+
 ${data.description}
+
 ## Table of contents
+
 - [Description](#Description)
 - [Installation](#Installation)
 - [Usage](#Usage)
@@ -26,19 +31,32 @@ ${data.description}
 - [Test](#Test)
 - [Repository Link](#Repository)
 - [GitHub Info](#GitHub) 
+
 ## Installation
+
         ${data.installation}
+
 ## Usage
+
 ${data.usage}
+
 ## Licence
+
 ${data.licence}
+
 ## Contributors
+
 ${data.contributing}
+
 ## Test
+
 ${data.test}
+
 ## Repository
+
 - [Project Repo](${data.repo})
 ## GitHub
+
 ![Image of me](${githubInfo.githubImage})
 - ${githubInfo.name}
 - [GitHub Profile](${githubInfo.profile})
